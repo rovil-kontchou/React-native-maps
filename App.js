f29bdab0ff7734299e39144e9fea44bd84c04199
rovil-kontchou <168017592+rovil-kontchou@users.noreply.github.com>
2024-09-27 23:18:38 +0100

    adding marker to a specific area

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 
 export default function App() {
+
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
+
+  const handleRegionChange = (newRegion) => {
+    setRegion(newRegion);
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView 
+      style={styles.map} 
+      region={region}
+      >
+        <Marker coordinate={region} title='Marker' />
+      </MapView>
     </View>
   );
 }
